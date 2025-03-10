8de58f448d9a083c98c57452f11d432ee9729c95
Philippe Moeckli <135646622+LeZderich@users.noreply.github.com>
2025-03-10 16:34:14 +0100

    chore: change the type of the app to module and publish migration files for the db

--- server/main.js
+++ server/main.js
@@ -13,7 +13,6 @@ app.get("/", async (req, res) => {
   res.status(200).json({ status: "OK" });
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
