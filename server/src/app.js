e2d6600c4f8923216eb0f010f21e1c4495734f0c
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2025-03-11 16:24:16 +0100

    chore: update dependencies and server configuration

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
+import cors from "cors";
 import homeworkRoutes from "./routes/homeworks.js";
 import subjectRoutes from "./routes/subjects.js";
 import stateRoutes from "./routes/states.js";
 
 const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.use(express.json());
 
