8432468a6f6a4235851b73e3b0fdd2ccd4e8f0d5
Philippe Moeckli <135646622+LeZderich@users.noreply.github.com>
2025-03-16 02:49:13 +0100

    chore: remove maxAge property from cookie configuration in auth.js

--- server/src/config/auth.js
+++ server/src/config/auth.js
@@ -11,7 +11,6 @@ export const authConfig = {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
     },
   },
 };
