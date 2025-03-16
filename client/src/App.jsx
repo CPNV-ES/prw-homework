b3de1a839f60404fd8295fd49b5104c3b2d22953
Philippe Moeckli <135646622+LeZderich@users.noreply.github.com>
2025-03-16 02:57:12 +0100

    feat: implement protected routes and integrate authentication in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,50 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import HomeworkList from './components/homework/HomeworkList';
-import HomeworkForm from './components/homework/HomeworkForm';
-import SubjectList from './components/subject/SubjectList';
-import SubjectForm from './components/subject/SubjectForm';
-import StateList from './components/state/StateList';
-import StateForm from './components/state/StateForm';
-import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomeworkList from "./components/homework/HomeworkList";
+import HomeworkForm from "./components/homework/HomeworkForm";
+import SubjectList from "./components/subject/SubjectList";
+import SubjectForm from "./components/subject/SubjectForm";
+import StateList from "./components/state/StateList";
+import StateForm from "./components/state/StateForm";
+import Home from "./components/Home";
+import Login from "./components/auth/Login";
+import DiscordCallback from "./components/auth/DiscordCallback";
+import { AuthProvider } from "./components/auth/AuthProvider";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container mt-4">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/homeworks" element={<HomeworkList />} />
-            <Route path="/homeworks/new" element={<HomeworkForm />} />
-            <Route path="/homeworks/edit/:id" element={<HomeworkForm />} />
-            <Route path="/subjects" element={<SubjectList />} />
-            <Route path="/subjects/new" element={<SubjectForm />} />
-            <Route path="/subjects/edit/:id" element={<SubjectForm />} />
-            <Route path="/states" element={<StateList />} />
-            <Route path="/states/new" element={<StateForm />} />
-            <Route path="/states/edit/:id" element={<StateForm />} />
-          </Routes>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container mt-4">
+            <Routes>
+              {/* Public routes */}
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/auth/discord/callback"
+                element={<DiscordCallback />}
+              />
+
+              {/* Protected route group */}
+              <Route element={<ProtectedRoute />}>
+                <Route path="/homeworks" element={<HomeworkList />} />
+                <Route path="/homeworks/new" element={<HomeworkForm />} />
+                <Route path="/homeworks/edit/:id" element={<HomeworkForm />} />
+                <Route path="/subjects" element={<SubjectList />} />
+                <Route path="/subjects/new" element={<SubjectForm />} />
+                <Route path="/subjects/edit/:id" element={<SubjectForm />} />
+                <Route path="/states" element={<StateList />} />
+                <Route path="/states/new" element={<StateForm />} />
+                <Route path="/states/edit/:id" element={<StateForm />} />
+              </Route>
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </AuthProvider>
   );
 }
 
---
+++ client/src/components/auth/ProtectedRoute.jsx
@@ -0,0 +1,27 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+
+function ProtectedRoute() {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <div className="container mt-5">
+        <div className="d-flex justify-content-center">
+          <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+        <p className="text-center mt-3">Loading...</p>
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+}
+
+export default ProtectedRoute;
