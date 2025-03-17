92d81828db9d943bfd149f4eb09ffa94bc415c98
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2025-03-17 14:37:57 +0100

    feat(database): add notification fields and homework state relation

---
+++ server/prisma/migrations/20250317125734_add_notification_fields/migration.sql
@@ -0,0 +1,20 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Homework" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "deadline" DATETIME NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "subjectId" INTEGER,
+    "notificationSent" BOOLEAN NOT NULL DEFAULT false,
+    "notificationThreshold" INTEGER NOT NULL DEFAULT 24,
+    CONSTRAINT "Homework_subjectId_fkey" FOREIGN KEY ("subjectId") REFERENCES "Subject" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Homework" ("createdAt", "deadline", "description", "id", "subjectId", "title", "updatedAt") SELECT "createdAt", "deadline", "description", "id", "subjectId", "title", "updatedAt" FROM "Homework";
+DROP TABLE "Homework";
+ALTER TABLE "new_Homework" RENAME TO "Homework";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
