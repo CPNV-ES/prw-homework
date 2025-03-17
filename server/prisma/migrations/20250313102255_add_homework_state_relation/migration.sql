0edeae738a8d2c6915f418faf11e403688400229
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2025-03-17 14:03:55 +0100

    feat(db): add migration for homework state relation

---
+++ server/prisma/migrations/20250313102255_add_homework_state_relation/migration.sql
@@ -0,0 +1,20 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Homework" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "deadline" DATETIME NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "subjectId" INTEGER,
+    "stateId" INTEGER,
+    CONSTRAINT "Homework_subjectId_fkey" FOREIGN KEY ("subjectId") REFERENCES "Subject" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Homework_stateId_fkey" FOREIGN KEY ("stateId") REFERENCES "State" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Homework" ("createdAt", "deadline", "description", "id", "subjectId", "title", "updatedAt") SELECT "createdAt", "deadline", "description", "id", "subjectId", "title", "updatedAt" FROM "Homework";
+DROP TABLE "Homework";
+ALTER TABLE "new_Homework" RENAME TO "Homework";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
